fe87c8206d92b4895d8c442ff2fb2c52b383d86c
Takanori Nakanowatari <nakanowatari@gmail.com>
2021-06-23 14:33:55 +0900

    792. Number of Matching Subsequences

---
+++ go/NumberofMatchingSubsequences/solution.go
@@ -0,0 +1,43 @@
+package number_match
+
+func numMatchingSubseq(s string, words []string) int {
+	m := make(map[rune][]int)
+	for i, c := range s {
+		if val, ok := m[c]; ok {
+			m[c] = append(val, i)
+		} else {
+			m[c] = []int{i}
+		}
+	}
+
+	ans := 0
+	for _, word := range words {
+		i := 0
+		var c rune
+		prevIdx := -1
+		idxFound := false
+		for i, c = range word {
+			if idxs, ok := m[c]; ok {
+				for _, idx := range idxs {
+					if idx > prevIdx {
+						prevIdx = idx
+						idxFound = true
+						break
+					}
+				}
+				if !idxFound {
+					i = 0
+					break
+				}
+			} else {
+				i = 0
+				break
+			}
+		}
+
+		if i == len(word)-1 {
+			ans += 1
+		}
+	}
+	return ans
+}
