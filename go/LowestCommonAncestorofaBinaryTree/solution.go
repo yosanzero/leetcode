2512261ece9e0da267f157e8d81a6c4ac76196f3
Takanori Nakanowatari <nakanowatari@gmail.com>
2021-07-01 15:38:34 +0900

    236. Lowest Common Ancestor of a Binary Tree

---
+++ go/LowestCommonAncestorofaBinaryTree/solution.go
@@ -0,0 +1,35 @@
+package lca
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil || root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil {
+		if right != nil {
+			return root
+		}
+		return left
+	}
+
+	return right
+
+}
